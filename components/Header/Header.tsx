8706aad35f141c0f5a5993bb912a42906a7cc235
Viktor <vv.zeipel@gmail.com>
2020-02-11 14:42:09 +0100

    SSR for graphql and nextjs

---
+++ components/Header/Header.tsx
@@ -0,0 +1,52 @@
+import { FunctionComponent as FC } from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Container, Row, Col } from 'react-grid-system';
+
+interface IHeaderProps {}
+interface ILinkTextProps {
+  readonly isactive?: boolean;
+}
+
+const Banner = styled.header`
+  background-color: white;
+  padding: 50px 0px;
+  text-align: center;
+`;
+
+const LinkText = styled.a<ILinkTextProps>`
+  font-size: 14px;
+  color: black;
+  margin-right: 15px;
+  cursor: pointer;
+  text-decoration: ${props => (props.isactive ? 'underline' : 'none')};
+`;
+
+const Header: FC<IHeaderProps> = () => {
+  const router = useRouter();
+  const { pathname } = router;
+
+  return (
+    <Banner>
+      <Container>
+        <nav>
+          <Row justify="center">
+            <Col xs={3}>
+              <Link href="/">
+                <LinkText isactive={pathname === '/'}>Home</LinkText>
+              </Link>
+            </Col>
+            <Col xs={3}>
+              <Link href="/about">
+                <LinkText isactive={pathname === '/about'}>About</LinkText>
+              </Link>
+            </Col>
+          </Row>
+        </nav>
+      </Container>
+    </Banner>
+  );
+};
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,63 @@
-import { AppProps } from 'next/app';
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, NormalizedCacheObject } from 'apollo-boost';
+import withApollo from '../hooks/withApollo';
 import { StateProvider, initialState } from '../lib/state';
 import reducer from '../reducers';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <Component {...pageProps} />
-    </StateProvider>
-  );
+export interface ITheme {
+  niceBlack: string;
 }
 
-export default MyApp;
+export interface IThemeWrapper {
+  theme: ITheme;
+}
+
+export const theme: ITheme = {
+  niceBlack: '#001F3F',
+};
+
+const GlobalStyle = createGlobalStyle<IThemeWrapper>`
+  * {
+    font-family: 'Arial';
+    margin: 0px;
+  }
+  body {
+    margin: 0 auto;
+    background-color: #f8f5f5;
+    color: ${props => props.theme.niceBlack}; 
+  }
+`;
+
+// since "apollo" isn't a native Next.js prop we have to declare it's type.
+interface IProps {
+  apollo: ApolloClient<NormalizedCacheObject>;
+}
+
+class MyApp extends App<IProps> {
+  render() {
+    const { Component, pageProps, apollo } = this.props;
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sök vecka</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <ApolloProvider client={apollo}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <StateProvider initialState={initialState} reducer={reducer}>
+              <Component {...pageProps} />
+            </StateProvider>
+          </ThemeProvider>
+        </ApolloProvider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withApollo(MyApp);
