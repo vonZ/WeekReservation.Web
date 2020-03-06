c9f26db52e90924a857a090e567c32494e0ad335
Viktor <vv.zeipel@gmail.com>
2020-03-07 00:02:35 +0100

    Deleted unused grapql queries and mutations

--- graphql/index.js
+++ graphql/index.js
@@ -1,22 +1,4 @@
-import { GET_ALL_RESERVATIONS } from './queries/getAllReservations';
-import { GET_ALL_CUSTOMERS } from './queries/getAllCustomers';
-import { GET_ALL_ROOM_TYPES } from './queries/getAllRoomTypes';
-import { GET_RESERVATION_BY_CUSTOMER_ID } from './queries/getReservationByCustomerId';
 import { GET_ALL_SLOTS } from './queries/getAllSlots';
 import { GET_SLOT_BY_DATESPAN } from './queries/getSlotByDateSpan';
 
-import { DELETE_RESERVATION } from './mutations/deleteReservation';
-import { MAKE_RESERVATION } from './mutations/makeReservation';
-import { CREATE_SLOT } from './mutations/createSlot';
-
-export {
-  GET_ALL_RESERVATIONS,
-  GET_ALL_CUSTOMERS,
-  GET_ALL_ROOM_TYPES,
-  GET_RESERVATION_BY_CUSTOMER_ID,
-  GET_ALL_SLOTS,
-  GET_SLOT_BY_DATESPAN,
-  DELETE_RESERVATION,
-  MAKE_RESERVATION,
-  CREATE_SLOT,
-};
+export { GET_ALL_SLOTS, GET_SLOT_BY_DATESPAN };
